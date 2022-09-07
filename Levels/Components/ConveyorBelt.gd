83dfea5e78506fee705ec90b75b8060e783d006d
Edwin Dierssen <Graggor@users.noreply.github.com>
2022-09-07 17:54:45 +0200

    Removed Duplicate Conveyor

--- Levels/Components/ConveyorBelt.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody2D
-
-export var speed = 100
-
-func _ready():
-	constant_linear_velocity.x = speed
-
-func _physics_process(delta):
-	$Sprite.texture.region.position.x -= speed * delta
