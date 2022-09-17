61a674afa317a60d3fc46ce7052e2c999e4f701c
Edwin Dierssen <Graggor@users.noreply.github.com>
2022-09-17 17:08:06 +0200

    fixed hiddenroom
    fixed hiddenroom not showing when entering

--- Levels/Hiddenroom.gd
+++ Levels/Hiddenroom.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
+var opened = false
+
 func _physics_process(delta):
 	 var bodies = get_overlapping_bodies()
 	 for body in bodies:
-			if body.name == "Player":
+			if body.name == "Player" and !opened:
+				opened = true
 				$AnimationPlayer.play("RoomRevealed")
 				yield($AnimationPlayer, "animation_finished")
-			else:
-				$AnimationPlayer.play("RoomHidden")
+			#else:
+				#$AnimationPlayer.play("RoomHidden")
