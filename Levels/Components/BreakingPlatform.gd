535b7d8047d440494f0f320409c4cea3bddaad31
Edwin Dierssen <Graggor@users.noreply.github.com>
2022-09-24 19:25:49 +0200

    BreakingPlatform reappear
    Made it so breakingplatforms reappear 5 seconds afte the animation is finished.

--- Levels/Components/BreakingPlatform.gd
+++ Levels/Components/BreakingPlatform.gd
@@ -6,7 +6,8 @@ func _on_BreakingPlatform_body_entered(body):
 	if body.name == "Player" and !opened:
 			opened = true
 			$AnimationPlayer.play("Destroyed")
-			yield($AnimationPlayer,"animation_finished")
-			queue_free()
-	else:
-		$AnimationPlayer.play("idle")
+			$RespawnTimer.start()
+
+func _on_RespawnTimer_timeout():
+	opened = false
+	$AnimationPlayer.play_backwards("Destroyed")
