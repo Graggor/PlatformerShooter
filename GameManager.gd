94ea64d0a160f50ec2afa6d71e54e39cbe283ada
Edwin Dierssen <Graggor@users.noreply.github.com>
2022-10-19 23:34:31 +0200

    Checkpoints
    Added checkpoints to each level
    Added basic change_level functionality in gamemanager
    Fade to black is currently reliant on a yield, please fix.  see https://selinadev.github.io/02-scene-transitions/#preparation

--- GameManager.gd
+++ GameManager.gd
@@ -4,6 +4,11 @@ export (PackedScene) var first_scene
 
 var time_start = 0
 var time_now = 0
+onready var level_holder = $Level
+onready var fade_rect = $FadeRect
+onready var tween = $Tween
+
+export var number_of_levels = 0
 
 signal timer_changed
 
@@ -21,6 +26,30 @@ func _process(delta):
 func start_game():
 	$MainMenu.hide()
 	$GUI.show()
-	var first_scene_loaded = first_scene.instance()
-	$Level.add_child(first_scene_loaded)
+	change_level(first_scene)
 	time_start = OS.get_unix_time()
+
+func change_level(scene: PackedScene):
+	fade(true)
+	#Remove current level
+	for level in level_holder.get_children():
+		level_holder.remove_child(level)
+		level.queue_free()
+	#Instance next level
+	fade(false)
+	yield(tween, "tween_all_completed")
+	var next_scene = scene.instance()
+	print(next_scene.has_signal("request_fade_to_black"))
+	print(next_scene.connect("request_fade_to_black", self, "fade"))
+	level_holder.add_child(next_scene)
+
+func fade(to_black = true):
+	var fade_duration = 0.5
+	if to_black:
+		tween.interpolate_property(fade_rect, "modulate:a", 1, 0, fade_duration)
+		tween.interpolate_callback(fade_rect, fade_duration, "hide")
+		tween.start()
+	else:
+		fade_rect.show()
+		tween.interpolate_property(fade_rect, "modulate:a", 0, 1, fade_duration)
+		tween.start()
--- Lampost.gd
+++ Lampost.gd
@@ -2,15 +2,16 @@ extends Node
 
 signal PlayerEntered
 export var flicker = true
+onready var animation_player = $Area2D/AnimationPlayer
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
 		if flicker:
-			$AnimationPlayer.play("Flashing")
+			animation_player.play("Flashing")
 		else:
-			$AnimationPlayer.play("On")
+			animation_player.play("On")
 	emit_signal("PlayerEntered")
 
 func _on_Area2D_body_exited(body):
 	if body.name == "Player":
-		$AnimationPlayer.play("Off")
+		animation_player.play("Off")
